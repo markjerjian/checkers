8ad8c8763660ff24645af8420ec5cfe43a241815
markjerjian <mark.jerjian@gmail.com>
2018-08-25 14:12:21 +0100

    add board class

---
+++ board.py
@@ -0,0 +1,17 @@
+class Board:
+    def __init__(self, size: int = 8):
+        self.size = 8
+        self.board = [[0] * self.size] * self.size
+
+    def __repr__(self):
+        return "Board({})".format(self.size)
+
+    def __str__(self):
+        return "\n".join([" ".join(str(square) for square in row)
+                          for row in self.board])
+
+
+if __name__ == "__main__":
+    board = Board(size=4)
+
+    print(board)
